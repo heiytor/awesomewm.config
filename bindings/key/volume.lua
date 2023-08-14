14f48fea26a715026dea7629e093bc2eb09503fe
Heitor Danilo <heitor.danilo@icloud.com>
2023-08-14 00:45:03 -0300

    widgets

--- bindings/key/volume.lua
+++ bindings/key/volume.lua
@@ -1,9 +1,9 @@
 -- "amixer" binary is in "alsa-plugins" package.
 
 awful.keyboard.append_global_keybindings({
-    awful.key({ MODKEY,           }, "[", function() awful.spawn("amixer -D pulse sset Master 5%-") end,
+    awful.key({ MODKEY,           }, "]", function() awful.spawn("amixer -D pulse sset Master 5%-") end,
               {description = "Decrease volume", group = "volume"}),
-    awful.key({ MODKEY,           }, "]", function() awful.spawn("amixer -D pulse sset Master 5%+") end,
+    awful.key({ MODKEY,           }, "[", function() awful.spawn("amixer -D pulse sset Master 5%+") end,
               {description = "Increase volume", group = "volume"}),
     awful.key({ MODKEY,           }, "/", function() awful.spawn("amixer -D pulse sset Master toggle", false) end,
               { description = "Mute volume", group = "Volume" }),
