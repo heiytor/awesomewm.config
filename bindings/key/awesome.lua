9fcba3b69c32c8e38d83a4b30a6ae318642f65a0
Heitor Danilo <heitor.danilo@icloud.com>
2023-11-17 00:29:43 -0300

    fmt: format code

--- bindings/key/awesome.lua
+++ bindings/key/awesome.lua
@@ -1,20 +1,13 @@
 Awful.keyboard.append_global_keybindings({
-    Awful.key({ MODKEY,           }, "s",      Hotkeys_popup.show_help,
-              {description="Show keybings", group="awesome"}),
-    -- awful.key({ MODKEY,           }, "w", function () mymainmenu:show() end,
-    --           {description = "Show menu", group = "awesome"}),
-    Awful.key({ MODKEY,           }, "r", awesome.restart,
-              {description = "Reload", group = "awesome"}),
-    Awful.key({ MODKEY, "Control" }, "q", awesome.quit,
-              {description = "Quit", group = "awesome"}),
-    Awful.key({ MODKEY            }, "x",
-              function ()
-                  Awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = Awful.screen.focused().mypromptbox.widget,
-                    exe_callback = Awful.util.eval,
-                    history_path = Awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
+	Awful.key({ MODKEY }, "s", Hotkeys_popup.show_help, { description = "Show keybings", group = "awesome" }),
+	Awful.key({ MODKEY }, "r", awesome.restart, { description = "Reload", group = "awesome" }),
+	Awful.key({ MODKEY, "Control" }, "q", awesome.quit, { description = "Quit", group = "awesome" }),
+	Awful.key({ MODKEY }, "x", function()
+		Awful.prompt.run({
+			prompt = "Run Lua code: ",
+			textbox = Awful.screen.focused().mypromptbox.widget,
+			exe_callback = Awful.util.eval,
+			history_path = Awful.util.get_cache_dir() .. "/history_eval",
+		})
+	end, { description = "lua execute prompt", group = "awesome" }),
 })
